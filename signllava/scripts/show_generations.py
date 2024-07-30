49bcf35518abacbaa603122968b1ee898fae3289
Este1le <estelle7w7@gmail.com>
2024-07-30 14:18:15 -0400

    add scripts to show generation results properly

--- signllava/scripts/show_generations.py
+++ signllava/scripts/show_generations.py
@@ -1,8 +1,8 @@
 import json
 
-nocontext = "generation.nocontext.json"
+nocontext = "generation.one_keyword.json"
 #prepared_predicted_context = "generation.prepared_predicted_context.json"
-on_the_fly_predicted_context = "generation.on_the_fly_predicted_context.json"
+#on_the_fly_predicted_context = "generation.on_the_fly_predicted_context.json"
 
 def get_json(json_file):
     with open(json_file) as f:
@@ -10,7 +10,7 @@ def get_json(json_file):
 
 nocontext = get_json(nocontext)
 #prepared_predicted_context = get_json(prepared_predicted_context)
-on_the_fly_predicted_context = get_json(on_the_fly_predicted_context)
+#on_the_fly_predicted_context = get_json(on_the_fly_predicted_context)
 
 for video in nocontext:
     print(nocontext[video]['clip_order'])
@@ -18,8 +18,13 @@ for video in nocontext:
         if clip_name in nocontext[video]:
             print(video + "      " + clip_name)
             #print(video + "-" + str(nocontext[video]['clip_order'].index(clip_name)))
-            print("[ref]:", nocontext[video][clip_name]['translation'])
+            if 'prompt' in nocontext[video][clip_name]:
+                print("[prompt]:", nocontext[video][clip_name]['prompt'])
+            if 'reference' in nocontext[video][clip_name]:
+                print("[ref]:", nocontext[video][clip_name]['reference'])
+            else:
+                print("[ref]:", nocontext[video][clip_name]['translation'])
             print("[nocontext]:", nocontext[video][clip_name]['hypothesis'])
             #print("[prepared_context]:", prepared_predicted_context[video][clip_name]['hypothesis'])
-            print("[on_the_fly_context]:", on_the_fly_predicted_context[video][clip_name]['hypothesis'])
+            #print("[on_the_fly_context]:", on_the_fly_predicted_context[video][clip_name]['hypothesis'])
             print("\n")

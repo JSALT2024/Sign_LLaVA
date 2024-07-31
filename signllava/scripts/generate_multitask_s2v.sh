32612c022b3d8a5d5e7bab111464051f9a2b31ac
karahan-sahin <karahan.sahin@boun.edu.tr>
2024-07-31 15:22:02 -0400

    chore: Update generate_multitask_s2v.yaml configuration file

--- signllava/scripts/generate_multitask_s2v.sh
+++ signllava/scripts/generate_multitask_s2v.sh
@@ -6,4 +6,4 @@ echo "Setting CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES}"
 ########### DO NOT CHANGE ###########
 #deepspeed --include localhost:${CUDA_ID} --master_port=29515 llava/eval/run_signllava.py \
 # --deepspeed ./scripts/zero2.json \
-python llava/eval/run_signllava_multi.py --yaml_args signllava/configs/truba/how2sign_finetune_s2v.yaml
+python llava/eval/run_signllava_multi.py --yaml_args signllava/configs/truba/generate_multitask_s2v.yaml

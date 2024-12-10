68bb38274f275d9edc0dbe5f4fca05bcbf2cbd78
zeleznyt <tomas@zelezny.name>
2024-12-10 09:10:46 +0200

    Fix lora bug

--- llava/train/train.py
+++ llava/train/train.py
@@ -618,8 +618,8 @@ def train(attn_implementation=None):
         bnb_model_from_pretrained_args.update(dict(
             #device_map="auto",
             device_map={"": training_args.device},
-            load_in_4bit=training_args.bits == 4,
-            load_in_8bit=training_args.bits == 8,
+            # load_in_4bit=training_args.bits == 4,
+            # load_in_8bit=training_args.bits == 8,
             quantization_config=BitsAndBytesConfig(
                 load_in_4bit=training_args.bits == 4,
                 load_in_8bit=training_args.bits == 8,
